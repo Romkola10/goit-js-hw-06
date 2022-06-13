3e3fe39e2b91db109a2a45e4ceb2d80e7adfc4fe
Romkola10 <57733600+Romkola10@users.noreply.github.com>
2022-06-13 21:09:19 +0300

    second_push

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+
+const addingItem = options => {
+  return options.map(option => {
+    const listItemEl = document.createElement('li');
+    listItemEl.textContent = option;
+    listItemEl.classList.add('item');
+    return listItemEl;
+  
+  });
+  
+  
+}
+const elements = addingItem(ingredients);
+listEl.append(...elements);
+
+
+
+
+
